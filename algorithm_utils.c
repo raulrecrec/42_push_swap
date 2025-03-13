faa5270001dc1a0b45d251de8c50bf17a9f18506
WSL HOME <raulrecrec@gmail.com>
2025-03-13 15:55:28 +0100

    Add new file sorting_utils.c and implement is_sorted function
    - Created sorting_utils.c to store sorting-related utility functions.
    - Implemented is_sorted function to check if a stack is sorted in ascending order.

---
+++ algorithm_utils.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 15:45:00 by rexposit          #+#    #+#             */
+/*   Updated: 2025/03/13 15:55:15 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_sorted(int *stack, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		if (stack[i] > stack[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
