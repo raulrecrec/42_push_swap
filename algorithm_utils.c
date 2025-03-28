508be2d89f224ce3fff084431d230223bdffc0ad
WSL HOME <raulrecrec@gmail.com>
2025-03-28 18:57:58 +0100

    Add new file algorithm_utils.c and implement sort_three
    - Created algorithm_utils.c to store small utility sorting algorithms.
    - Implemented sort_three to handle sorting exactly three elements with minimal operations.

--- algorithm_utils.c
+++ algorithm_utils.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:45:00 by rexposit          #+#    #+#             */
-/*   Updated: 2025/03/13 15:55:15 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/03/28 18:57:54 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,31 @@ int	is_sorted(int *stack, int size)
 		i++;
 	}
 	return (1);
+}
+
+void	sort_three(int *stack_a, int size)
+{
+	if ((stack_a[0] < stack_a[1]) && (stack_a[1] < stack_a[2])) // caso 1
+		return ;
+	else if ((stack_a[0] < stack_a[1]) && (stack_a[1] > stack_a[2])) // caso 2
+	{
+		reverse_rotate_a(stack_a, size);
+		swap_a(stack_a, size);
+	}
+	else if ((stack_a[0] > stack_a[1]) && (stack_a[0] < stack_a[2])) // caso 3
+		swap_a(stack_a, size);
+	else if ((stack_a[0] > stack_a[2]) && (stack_a[0] < stack_a[1])) // caso 4
+	{
+		rotate_a(stack_a, size);
+		swap_a(stack_a, size);
+	}
+	else if ((stack_a[0] > stack_a[1]) && (stack_a[1] < stack_a[2])) // caso 5
+	{
+		rotate_a(stack_a, size);
+	}
+	else if ((stack_a[0] > stack_a[1]) && (stack_a[1] > stack_a[2])) // caso 6
+	{
+		rotate_a(stack_a, size);
+		swap_a(stack_a, size);
+	}
 }
