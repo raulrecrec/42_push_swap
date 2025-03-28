a61f7b47415f0dd2b58c7a800e57fd17f71231b5
WSL HOME <raulrecrec@gmail.com>
2025-03-28 19:14:02 +0100

    Update sort_three to remove unnecessary case for sorted stack
    - Removed the first condition that checked if the stack was already sorted.
    - That check is now handled earlier in push_swap, simplifying the function.

--- algorithm_utils.c
+++ algorithm_utils.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:45:00 by rexposit          #+#    #+#             */
-/*   Updated: 2025/03/28 18:57:54 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/03/28 19:13:13 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,7 @@ int	is_sorted(int *stack, int size)
 
 void	sort_three(int *stack_a, int size)
 {
-	if ((stack_a[0] < stack_a[1]) && (stack_a[1] < stack_a[2])) // caso 1
-		return ;
-	else if ((stack_a[0] < stack_a[1]) && (stack_a[1] > stack_a[2])) // caso 2
+	if ((stack_a[0] < stack_a[1]) && (stack_a[1] > stack_a[2])) // caso 2
 	{
 		reverse_rotate_a(stack_a, size);
 		swap_a(stack_a, size);
