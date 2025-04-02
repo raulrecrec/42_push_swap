78afcf02f67a78d57408d4f5d9ce2cdf5671d1d6
WSL HOME <raulrecrec@gmail.com>
2025-04-02 02:44:16 +0200

    Update algorithm_utils.c to implement sort_five and supporting functions
    - Implemented full sort_five logic for both 4 and 5 element stacks.
    - Added helper functions push_min_to_b_case4 and push_min_to_b_case5.
    - Modified find_min_stack to return both value and index of minimum element.
    - Adjusted sort_three to account for minimal conditions and size.

--- algorithm_utils.c
+++ algorithm_utils.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:45:00 by rexposit          #+#    #+#             */
-/*   Updated: 2025/04/01 16:12:28 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/04/02 02:32:44 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,19 @@ int	is_sorted(int *stack, int size)
 	return (1);
 }
 
-void	find_min_stack(int *stack, int size, int *min_value, int *min_idex)
+void	find_min_stack(int *stack, int size, int *min_value, int *min_index)
 {
 	int	i;
 
 	*min_value = stack[0];
-	*min_idex = 0;
+	*min_index = 0;
 	i = 1;
 	while (i < size)
 	{
 		if (stack[i] < *min_value)
 		{
 			*min_value = stack[i];
-			*min_idex = i;
+			*min_index = i;
 		}
 		i++;
 	}
@@ -69,7 +69,83 @@ void	sort_three(int *stack_a, int size)
 	}
 }
 
-void	sort_five(int *stack_a, int size)
+void	push_min_to_b_case4(int **stack_a, int **stack_b, int *size_a, int *size_b, int min_index)
 {
-	
+	if (min_index == 0)
+		push_b(stack_a, stack_b, size_a, size_b);
+	else if (min_index == 1)
+	{
+		swap_a(*stack_a, *size_a);
+		push_b(stack_a, stack_b, size_a, size_b);
+	}
+	else if (min_index == 2)
+	{
+		rotate_a(*stack_a, *size_a);
+		swap_a(*stack_a, *size_a);
+		push_b(stack_a, stack_b, size_a, size_b);
+	}
+	else if (min_index == 3)
+	{
+		reverse_rotate_a(*stack_a, *size_a);
+		push_b(stack_a, stack_b, size_a, size_b);
+	}
+	else if (min_index == 4)
+	{
+		reverse_rotate_a(*stack_a, *size_a);
+		push_b(stack_a, stack_b, size_a, size_b);
+	}
+}
+
+void	push_min_to_b_case5(int **stack_a, int **stack_b, int *size_a, int *size_b, int min_index)
+{
+	if (min_index == 0)
+		push_b(stack_a, stack_b, size_a, size_b);
+	else if (min_index == 1)
+	{
+		swap_a(*stack_a, *size_a);
+		push_b(stack_a, stack_b, size_a, size_b);
+	}
+	else if (min_index == 2)
+	{
+		rotate_a(*stack_a, *size_a);
+		swap_a(*stack_a, *size_a);
+		push_b(stack_a, stack_b, size_a, size_b);
+	}
+	if (min_index == 3)
+	{
+		reverse_rotate_a(*stack_a, *size_a);
+		reverse_rotate_a(*stack_a, *size_a);
+		push_b(stack_a, stack_b, size_a, size_b);
+	}
+	else if (min_index == 4)
+	{
+		reverse_rotate_a(*stack_a, *size_a);
+		push_b(stack_a, stack_b, size_a, size_b);
+	}
+}
+
+void	sort_five(int **stack_a, int **stack_b, int *size_a, int *size_b)
+{
+	int	i;
+	int	min_value;
+	int	min_index;
+
+	if (*size_a == 4)
+	{
+		find_min_stack(*stack_a, *size_a, &min_value, &min_index);
+		push_known_min_to_b_case4(stack_a, stack_b, size_a, size_b, min_index);
+		sort_three(*stack_a, *size_a - 1);
+		push_a(*stack_a, *stack_b, *size_a - 1, *size_b + 1);
+	}
+	if (*size_a == 5)
+	{
+		find_min_stack(*stack_a, *size_a, &min_value, &min_index);
+		push_known_min_to_b_case5(stack_a, stack_b, size_a, size_b, min_index);
+		find_min_stack(*stack_a, *size_a, &min_value, &min_index);
+		push_known_min_to_b_case5(stack_a, stack_b, size_a, size_b, min_index);
+		sort_three(*stack_a, *size_a - 2);
+		push_a(*stack_a, *stack_b, *size_a - 1, *size_b + 2);
+		push_a(*stack_a, *stack_b, *size_a - 1, *size_b + 1);
+		swap_a(*stack_a, *size_a);
+	}
 }
