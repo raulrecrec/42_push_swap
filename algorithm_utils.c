184194b454257976b21e4f9d00cf48d508692d03
WSL HOME <raulrecrec@gmail.com>
2025-04-09 13:40:11 +0200

    Update sort_three logic to correctly handle all 3-element permutations
    - Rewrote sort_three to cover all six permutations using clear conditional logic.
    - Ensures correct sorting for stack sizes of 3 with optimized operations.
    - Replaces previous logic with updated condition set based on element comparisons.

--- algorithm_utils.c
+++ algorithm_utils.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:45:00 by rexposit          #+#    #+#             */
-/*   Updated: 2025/04/09 12:23:44 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/04/09 13:31:34 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void	find_min_stack(int *stack, int size, int *min_index)
 
 void	sort_three(int *stack_a, int size)
 {
-	if ((stack_a[0] < stack_a[1]) && (stack_a[1] > stack_a[2])) // caso 2
+	if ((stack_a[0] < stack_a[1]) && (stack_a[0] < stack_a[2])) // caso 2
 	{
 		reverse_rotate_a(stack_a, size);
 		swap_a(stack_a, size);
@@ -57,7 +57,7 @@ void	sort_three(int *stack_a, int size)
 	else if ((stack_a[0] > stack_a[2]) && (stack_a[0] < stack_a[1])) // caso 4
 	{
 		rotate_a(stack_a, size);
-		swap_a(stack_a, size);
+		rotate_a(stack_a, size);
 	}
 	else if ((stack_a[0] > stack_a[1]) && (stack_a[1] < stack_a[2])) // caso 5
 	{
