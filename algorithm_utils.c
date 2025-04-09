bdbbefe736e5eae6b186d503d24bddd727fe4c6b
WSL HOME <raulrecrec@gmail.com>
2025-04-09 15:05:35 +0200

    Update sort_three to handle already sorted stacks
    - Added an initial check using is_sorted to skip operations if the stack is already sorted.
    - Prevents unnecessary moves and ensures correctness for all 3-element and 4-element permutations.

--- algorithm_utils.c
+++ algorithm_utils.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:45:00 by rexposit          #+#    #+#             */
-/*   Updated: 2025/04/09 13:31:34 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/04/09 15:02:23 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,9 @@ void	find_min_stack(int *stack, int size, int *min_index)
 
 void	sort_three(int *stack_a, int size)
 {
-	if ((stack_a[0] < stack_a[1]) && (stack_a[0] < stack_a[2])) // caso 2
+	if (is_sorted(stack_a, size))
+		return ;
+	else if ((stack_a[0] < stack_a[1]) && (stack_a[0] < stack_a[2])) // caso 2
 	{
 		reverse_rotate_a(stack_a, size);
 		swap_a(stack_a, size);
