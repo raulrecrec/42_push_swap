a12ba4a24587a958ea9f4f85a33baa88695c6333
WSL HOME <raulrecrec@gmail.com>
2025-04-12 20:40:45 +0200

    Refactor radix_sort.c to fix leaks and correct index conversion
    - Rewrote convert_to_indexed() to use a separate copy buffer and avoid self-overwriting issues.
    - Removed outdated in-place modifications of the indexed array to mirror stack operations.
    - Now converts stack_a into an indexed version and replaces its contents safely.
    - Added memory safety checks for allocation and freeing copy buffers.
    - Used get_max_bits() for calculating radix iterations instead of get_max_value().
    - Cleaned up and removed debug prints from the final version.

--- radix_sort.c
+++ radix_sort.c
@@ -6,23 +6,29 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 03:59:39 by rexposit          #+#    #+#             */
-/*   Updated: 2025/04/08 02:22:21 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/04/12 20:40:37 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	get_max_value(int *stack, int size)
+int	get_max_bits(int *stack, int size)
 {
-	int	i = 0;
-	int	max = stack[0];
+	int	i;
+	int	max_value;
+	int max_bits;
 
+	i = 0;
+	max_value = stack[0];
 	while (++i < size)
 	{
-		if (stack[i] > max)
-			max = stack[i];
+		if (stack[i] > max_value)
+			max_value = stack[i];
 	}
-	return (max);
+	max_bits = 0;
+	while ((max_value >> max_bits) != 0)
+		max_bits++;
+	return (max_bits);
 }
 
 int	*convert_to_indexed(int *stack, int size)
@@ -30,28 +36,30 @@ int	*convert_to_indexed(int *stack, int size)
 	int	i;
 	int	j;
 	int	index;
-	int	*stack_indexed;
+	int	*copy;
+	int	*indexed;
 
-	stack_indexed = malloc(size * sizeof(int));
-	if (!stack_indexed)
+	copy = malloc(size * sizeof(int));
+	indexed = malloc(size * sizeof(int));
+	if (!copy || !indexed)
 		return (NULL);
-	ft_memcpy(stack_indexed, stack, size * sizeof(int));
+	ft_memcpy(copy, stack, size * sizeof(int));
 	i = 0;
-	j = 0;
 	while (i < size)
 	{
 		index = 0;
 		j = 0;
 		while (j < size)
 		{
-			if (stack_indexed[i] > stack_indexed[j])
+			if (copy[i] > copy[j])
 				index++;
 			j++;
 		}
-		stack_indexed[i] = index;
+		indexed[i] = index;
 		i++;
 	}
-	return (stack_indexed);
+	free(copy);
+	return (indexed);
 }
 
 void	radix_sort(int **stack_a, int **stack_b, int *size_a, int *size_b)
@@ -61,41 +69,30 @@ void	radix_sort(int **stack_a, int **stack_b, int *size_a, int *size_b)
 	int	j;
 	int	bit;
 	int	max_bits;
+	int	size;
 
 	indexed = convert_to_indexed(*stack_a, *size_a);
 	if (!indexed)
 		return ;
-
-	max_bits = 0;
-	while ((get_max_value(indexed, *size_a) >> max_bits) != 0)
-		max_bits++;
-
+	free(*stack_a);
+	*stack_a = indexed;
+	max_bits = get_max_bits(*stack_a, *size_a);
 	i = 0;
 	while (i < max_bits)
 	{
 		j = 0;
-		while (j < *size_a)
+		size = *size_a;
+		while (j < size)
 		{
-			bit = (indexed[0] >> i) & 1;
+			bit = ((*stack_a)[0] >> i) & 1;
 			if (bit == 0)
-			{
 				push_b(stack_a, stack_b, size_a, size_b);
-				// reflejar movimiento también en indexed
-				ft_memmove(indexed, indexed + 1, (*size_a) * sizeof(int));
-			}
 			else
-			{
 				rotate_a(*stack_a, *size_a);
-				// rotamos también indexed
-				int tmp = indexed[0];
-				ft_memmove(indexed, indexed + 1, (*size_a - 1) * sizeof(int));
-				indexed[*size_a - 1] = tmp;
-			}
 			j++;
 		}
 		while (*size_b > 0)
 			push_a(stack_a, stack_b, size_a, size_b);
 		i++;
 	}
-	free(indexed);
 }
