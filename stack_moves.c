71dc145866e1357c18a77f416cb9f4c3e42c7675
WSL HOME <raulrecrec@gmail.com>
2025-04-13 05:27:23 +0200

    Update stack_moves.c to operate directly with double pointers
    - Modified swap, push, rotate and reverse_rotate functions to use **stack_a and **stack_b.
    - Ensured memory-safe operations and updated all assignments accordingly.

--- stack_moves.c
+++ stack_moves.c
@@ -6,39 +6,39 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 19:26:02 by rexposit          #+#    #+#             */
-/*   Updated: 2025/04/09 17:50:01 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/04/12 23:24:49 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	swap_a(int *stack_a, int size)
+int	swap_a(int **stack_a, int size)
 {
 	int	aux;
 
-	if (!stack_a || size < 2)
+	if (!*stack_a || size < 2)
 		return (0);
-	aux = stack_a[0];
-	stack_a[0] = stack_a[1];
-	stack_a[1] = aux;
+	aux = *stack_a[0];
+	(*stack_a)[0] = (*stack_a)[1];
+	(*stack_a)[1] = aux;
 	ft_printf("sa\n");
 	return (1);
 }
-int	swap_b(int *stack_b, int size)
+int	swap_b(int **stack_b, int size)
 {
 	int	aux;
 
-	if (!stack_b || size < 2)
+	if (!*stack_b || size < 2)
 		return (0);
-	aux = stack_b[0];
-	stack_b[0] = stack_b[1];
-	stack_b[1] = aux;
+	aux = (*stack_b)[0];
+	(*stack_b)[0] = (*stack_b)[1];
+	(*stack_b)[1] = aux;
 	ft_printf("sb\n");
 	return (1);
 }
-int	swap_ab(int *stack_a, int *stack_b, int size_a, int size_b)
+int	swap_ab(int **stack_a, int **stack_b, int size_a, int size_b)
 {
-	if (!stack_a || !stack_b || size_a < 2 || size_b < 2)
+	if (!*stack_a || !*stack_b || size_a < 2 || size_b < 2)
 		return (0);
 	swap_a(stack_a, size_a);
 	swap_b(stack_b, size_b);
@@ -98,66 +98,66 @@ int	push_b(int **stack_a, int **stack_b, int *size_a, int *size_b)
 	return (1);
 }
 
-void	rotate_a(int *stack_a, int size_a)
+void	rotate_a(int **stack_a, int size_a)
 {
 	int	aux;
 
-	if (!stack_a || size_a <= 1)
+	if (!*stack_a || size_a <= 1)
 		return ;
-	aux = stack_a[0];
-	ft_memmove(stack_a, stack_a + 1, (size_a - 1) * sizeof(int));
-	stack_a[size_a - 1] = aux;
+	aux = (*stack_a)[0];
+	ft_memmove(*stack_a, *stack_a + 1, (size_a - 1) * sizeof(int));
+	(*stack_a)[size_a - 1] = aux;
 	ft_printf("ra\n");
 }
 
-void	rotate_b(int *stack_b, int size_b)
+void	rotate_b(int **stack_b, int size_b)
 {
 	int	aux;
 
-	if (!stack_b || size_b <= 1)
+	if (!*stack_b || size_b <= 1)
 		return ;
-	aux = stack_b[0];
-	ft_memmove(stack_b, stack_b + 1, (size_b - 1) * sizeof(int));
-	stack_b[size_b - 1] = aux;
+	aux = (*stack_b)[0];
+	ft_memmove(*stack_b, *stack_b + 1, (size_b - 1) * sizeof(int));
+	(*stack_b)[size_b - 1] = aux;
 	ft_printf("rb\n");
 }
 
-void	rotate_ab(int *stack_a, int *stack_b, int size_a, int size_b)
+void	rotate_ab(int **stack_a, int **stack_b, int size_a, int size_b)
 {
-	if ((!stack_a || size_a <= 1) && (!stack_b || size_b <= 1))
+	if ((!*stack_a || size_a <= 1) && (!*stack_b || size_b <= 1))
 		return ;
 	rotate_a(stack_a, size_a);
 	rotate_b(stack_b, size_b);
 	ft_printf("rr\n");
 }
 
-void	reverse_rotate_a(int *stack_a, int size_a)
+void	reverse_rotate_a(int **stack_a, int size_a)
 {
 	int	aux;
 
-	if (!stack_a || size_a <= 1)
+	if (!*stack_a || size_a <= 1)
 		return ;
-	aux = stack_a[size_a - 1];
-	ft_memmove(stack_a + 1, stack_a, (size_a - 1) * sizeof(int));
-	stack_a[0] = aux;
+	aux = (*stack_a)[size_a - 1];
+	ft_memmove(*stack_a + 1, *stack_a, (size_a - 1) * sizeof(int));
+	(*stack_a)[0] = aux;
 	ft_printf("rra\n");
 }
 
-void	reverse_rotate_b(int *stack_b, int size_b)
+void	reverse_rotate_b(int **stack_b, int size_b)
 {
 	int	aux;
 
-	if (!stack_b || size_b <= 1)
+	if (!*stack_b || size_b <= 1)
 		return ;
-	aux = stack_b[size_b - 1];
-	ft_memmove(stack_b + 1, stack_b, (size_b - 1) * sizeof(int));
-	stack_b[0] = aux;
+	aux = (*stack_b)[size_b - 1];
+	ft_memmove(*stack_b + 1, *stack_b, (size_b - 1) * sizeof(int));
+	(*stack_b)[0] = aux;
 	ft_printf("rrb\n");
 }
 
-void	reverse_rotate_ab(int *stack_a, int *stack_b, int size_a, int size_b)
+void	reverse_rotate_ab(int **stack_a, int **stack_b, int size_a, int size_b)
 {
-	if ((!stack_a || size_a <= 1) && (!stack_b || size_b <= 1))
+	if ((!*stack_a || size_a <= 1) && (!*stack_b || size_b <= 1))
 		return ;
 	reverse_rotate_a(stack_a, size_a);
 	reverse_rotate_b(stack_b, size_b);
