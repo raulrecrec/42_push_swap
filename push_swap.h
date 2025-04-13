59a843208e13b9fcc0714a9aea18e99726a1895b
WSL HOME <raulrecrec@gmail.com>
2025-04-13 05:29:44 +0200

    Update push_swap.h to include get_max_bits
    - Replaced get_max_value with get_max_bits to reflect the final implementation of radix_sort.
    - Ensured proper prototype declarations for all current functions.

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 17:38:08 by rexposit          #+#    #+#             */
-/*   Updated: 2025/04/08 02:18:00 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/04/13 05:28:49 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include "42_libft/libft.h"
 
 // push_swap.c 
-void		push_swap(int *stack_a, int size_a);
+void		push_swap(int **stack_a, int size_a);
 long long	ft_atoll(const char *nptr);
 void		validate_arguments(int argc, char **argv);
 void		clean_exit(int **stack);
@@ -27,28 +27,28 @@ void		check_duplicates(int **stack, int size);
 int			*argv_to_int(int argc, char **argv);
 
 // stack_moves.c
-int			swap_a(int *stack_a, int size);
-int			swap_b(int *stack_b, int size);
-int			swap_ab(int *stack_a, int *stack_b, int size_a, int size_b);
+int			swap_a(int **stack_a, int size);
+int			swap_b(int **stack_b, int size);
+int			swap_ab(int **stack_a, int **stack_b, int size_a, int size_b);
 int			push_a(int **stack_a, int **stack_b, int *size_a, int *size_b);
 int			push_b(int **stack_a, int **stack_b, int *size_a, int *size_b);
-void		rotate_a(int *stack_a, int size_a);
-void		rotate_b(int *stack_b, int size_b);
-void		rotate_ab(int *stack_a, int *stack_b, int size_a, int size_b);
-void		reverse_rotate_a(int *stack_a, int size_a);
-void		reverse_rotate_b(int *stack_b, int size_b);
-void		reverse_rotate_ab(int *stack_a, int *stack_b, int size_a, int size_b);
+void		rotate_a(int **stack_a, int size_a);
+void		rotate_b(int **stack_b, int size_b);
+void		rotate_ab(int **stack_a, int **stack_b, int size_a, int size_b);
+void		reverse_rotate_a(int **stack_a, int size_a);
+void		reverse_rotate_b(int **stack_b, int size_b);
+void		reverse_rotate_ab(int **stack_a, int **stack_b, int size_a, int size_b);
 
 // algorithm_utils.c
 int			is_sorted(int *stack, int size);
-void		sort_three(int *stack_a, int size);
+void		sort_three(int **stack_a, int size);
 void		sort_five(int **stack_a, int **stack_b, int *size_a, int *size_b);
 void		find_min_stack(int *stack, int size, int *min_index);
 void		push_min_to_b_case4(int **stack_a, int **stack_b, int *size_a, int *size_b, int min_index);
 void		push_min_to_b_case5(int **stack_a, int **stack_b, int *size_a, int *size_b, int min_index);
 
-//radix_sort.c
-int			get_max_value(int *stack, int size);
+// radix_sort.c
+int			get_max_bits(int *stack, int size);
 int			*convert_to_indexed(int *stack, int size);
 void		radix_sort(int **stack_a, int **stack_b, int *size_a, int *size_b);
 
