ad7e138e45ced2c9000710bc6060ed082a44fc0a
WSL HOME <raulrecrec@gmail.com>
2025-04-13 05:25:08 +0200

    Update algorithm_utils.c to use double pointers in sorting functions
    - Updated sort_three to accept a double pointer to stack_a.
    - Modified push_min_to_b_case4 and push_min_to_b_case5 to receive and use size with pointer dereference.
    - Adapted sort_five logic to work directly with **stack_a and match new function signatures.

--- algorithm_utils.c
+++ algorithm_utils.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:45:00 by rexposit          #+#    #+#             */
-/*   Updated: 2025/04/09 15:02:23 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/04/13 03:11:19 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,27 +45,27 @@ void	find_min_stack(int *stack, int size, int *min_index)
 	}
 }
 
-void	sort_three(int *stack_a, int size)
+void	sort_three(int **stack_a, int size)
 {
-	if (is_sorted(stack_a, size))
+	if (is_sorted(*stack_a, size))
 		return ;
-	else if ((stack_a[0] < stack_a[1]) && (stack_a[0] < stack_a[2])) // caso 2
+	else if (((*stack_a)[0] < (*stack_a)[1]) && ((*stack_a)[0] < (*stack_a)[2]))
 	{
 		reverse_rotate_a(stack_a, size);
 		swap_a(stack_a, size);
 	}
-	else if ((stack_a[0] > stack_a[1]) && (stack_a[0] < stack_a[2])) // caso 3
+	else if (((*stack_a)[0] > (*stack_a)[1]) && ((*stack_a)[0] < (*stack_a)[2]))
 		swap_a(stack_a, size);
-	else if ((stack_a[0] > stack_a[2]) && (stack_a[0] < stack_a[1])) // caso 4
+	else if (((*stack_a)[0] > (*stack_a)[2]) && ((*stack_a)[0] < (*stack_a)[1]))
 	{
 		rotate_a(stack_a, size);
 		rotate_a(stack_a, size);
 	}
-	else if ((stack_a[0] > stack_a[1]) && (stack_a[1] < stack_a[2])) // caso 5
+	else if (((*stack_a)[0] > (*stack_a)[1]) && ((*stack_a)[1] < (*stack_a)[2]))
 	{
 		rotate_a(stack_a, size);
 	}
-	else if ((stack_a[0] > stack_a[1]) && (stack_a[1] > stack_a[2])) // caso 6
+	else if (((*stack_a)[0] > (*stack_a)[1]) && ((*stack_a)[1] > (*stack_a)[2]))
 	{
 		rotate_a(stack_a, size);
 		swap_a(stack_a, size);
@@ -78,22 +78,21 @@ void	push_min_to_b_case4(int **stack_a, int **stack_b, int *size_a, int *size_b,
 		push_b(stack_a, stack_b, size_a, size_b);
 	else if (min_index == 1)
 	{
-		swap_a(*stack_a, *size_a);
+		rotate_a(stack_a, *size_a);
 		push_b(stack_a, stack_b, size_a, size_b);
 	}
 	else if (min_index == 2)
 	{
-		rotate_a(*stack_a, *size_a);
-		swap_a(*stack_a, *size_a);
+		rotate_a(stack_a, *size_a);
+		rotate_a(stack_a, *size_a);
 		push_b(stack_a, stack_b, size_a, size_b);
 	}
 	else if (min_index == 3 || min_index == 4)
 	{
-		reverse_rotate_a(*stack_a, *size_a);
+		reverse_rotate_a(stack_a, *size_a);
 		push_b(stack_a, stack_b, size_a, size_b);
 	}
 }
-
 
 void	push_min_to_b_case5(int **stack_a, int **stack_b, int *size_a, int *size_b, int min_index)
 {
@@ -101,24 +100,24 @@ void	push_min_to_b_case5(int **stack_a, int **stack_b, int *size_a, int *size_b,
 		push_b(stack_a, stack_b, size_a, size_b);
 	else if (min_index == 1)
 	{
-		swap_a(*stack_a, *size_a);
+		rotate_a(stack_a, *size_a);
 		push_b(stack_a, stack_b, size_a, size_b);
 	}
 	else if (min_index == 2)
 	{
-		rotate_a(*stack_a, *size_a);
-		swap_a(*stack_a, *size_a);
+		rotate_a(stack_a, *size_a);
+		rotate_a(stack_a, *size_a);
 		push_b(stack_a, stack_b, size_a, size_b);
 	}
 	else if (min_index == 3)
 	{
-		reverse_rotate_a(*stack_a, *size_a);
-		reverse_rotate_a(*stack_a, *size_a);
+		reverse_rotate_a(stack_a, *size_a);
+		reverse_rotate_a(stack_a, *size_a);
 		push_b(stack_a, stack_b, size_a, size_b);
 	}
 	else if (min_index == 4)
 	{
-		reverse_rotate_a(*stack_a, *size_a);
+		reverse_rotate_a(stack_a, *size_a);
 		push_b(stack_a, stack_b, size_a, size_b);
 	}
 }
@@ -131,7 +130,7 @@ void	sort_five(int **stack_a, int **stack_b, int *size_a, int *size_b)
 	{
 		find_min_stack(*stack_a, *size_a, &min_index);
 		push_min_to_b_case4(stack_a, stack_b, size_a, size_b, min_index);
-		sort_three(*stack_a, *size_a);
+		sort_three(stack_a, *size_a);
 		push_a(stack_a, stack_b, size_a, size_b);
 	}
 	else if (*size_a == 5)
@@ -140,9 +139,8 @@ void	sort_five(int **stack_a, int **stack_b, int *size_a, int *size_b)
 		push_min_to_b_case5(stack_a, stack_b, size_a, size_b, min_index);
 		find_min_stack(*stack_a, *size_a, &min_index);
 		push_min_to_b_case4(stack_a, stack_b, size_a, size_b, min_index);
-		sort_three(*stack_a, *size_a);
+		sort_three(stack_a, *size_a);
 		push_a(stack_a, stack_b, size_a, size_b);
 		push_a(stack_a, stack_b, size_a, size_b);
 	}
 }
-
